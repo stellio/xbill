eb7307927734a00c173ebcde5d4b30571be509cc
Igor Lisovoy <igor.lisovoy@yandex.ru>
2016-07-21 00:44:27 +0300

    disable serial column for CouponPack and Contractor index page

--- backend/views/coupon-pack/index.php
+++ backend/views/coupon-pack/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            // ['class' => 'yii\grid\SerialColumn'],
 
             // 'id',
             [
